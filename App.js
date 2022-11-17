a57818d75d3d53fb0b409efb85a7912e97718923
rusty-gahoi <vipulrocks28@gmail.com>
2022-11-17 16:06:13 +0530

    mobile nav

--- App.js
+++ App.js
@@ -1,1 +1,27 @@
 
+
+let images = document.querySelectorAll('.content');   //gives a nodelist of all image tags
+let icons = document.querySelectorAll('nav ul li');
+
+icons.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllImages();
+        hideAllIcons();
+
+        item.classList.add('active');
+        images[idx].classList.add('show');
+        
+    })
+})
+
+function hideAllImages() {
+    images.forEach((img) => {
+        img.classList.remove('show');
+    })
+}
+
+function hideAllIcons() {
+    icons.forEach((item) => {
+        item.classList.remove('active');
+    })
+}
